120096c1627bcdab4859e612c484cb7492119cf2
andrewgremlich <andrew.gremlich@gmail.com>
2016-12-06 12:56:53 -0700

    reading files works.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,31 @@
 (function () {
 
+    var fs = require('fs')
+
+    console.log(fs)
+
     wand("#exportpackage").tag.onchange = e => {
-        let fileText = document.createTextNode(" - File Attached")
+
+        var ele = e.target || e.srcElement,
+            files = ele.files,
+            fileText
+
+        for (var i = 0; i < files.length; i++) {
+            var filepath = files[i].path
+
+            fs.readFile(filepath, 'utf8', (err, data) => {
+                if (err) throw err
+                console.log(data)
+            })
+
+        }
+
+        if (files.length === 1) {
+            fileText = document.createTextNode(" - File Attached")
+        } else {
+            fileText = document.createTextNode(" - Files Attached")
+        }
+
         e.target.parentElement.appendChild(fileText)
     }
 
