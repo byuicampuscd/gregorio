c87eb90a0f9a749361299aa6d695f4b596b0fee7
andrewgremlich <andrew.gremlich@gmail.com>
2016-12-09 14:51:24 -0700

    added dates to the inputs for possible quicker adjustment.

--- public/scripts/appender.js
+++ public/scripts/appender.js
@@ -2,11 +2,22 @@ var $ = require('jquery')
 
 function appenderSection(a, b, c) {
     var readabledate = new Date(a),
-        appender = $(`<div class="assignmentLabel">
+        date = readabledate.getDate(),
+        year = readabledate.getFullYear(),
+        month = readabledate.getMonth() + 1,
+        appender
+
+    if (date < 10)
+        date = "0" + date
+
+    if (month < 10)
+        month = "0" + month
+
+    appender = $(`<div class="assignmentLabel">
                         <label>
                             <p>Assignment: ${c}</p>
                             <p>Date: ${readabledate}</p>
-                            <input type="date">
+                            <input type="date" value="${year}-${month}-${date}">
                         </label>
                       </div>`)
     b.append(appender)
